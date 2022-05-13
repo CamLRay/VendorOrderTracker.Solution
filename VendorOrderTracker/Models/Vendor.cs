de0db6037eeb8d91f476ea243080ed42f8c0d0a2
Sploob <crayiphone@gmail.com>
2022-05-13 09:16:40 -0700

    first test passing

---
+++ VendorOrderTracker/Models/Vendor.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    
+
+    public Vendor(string name)
+    {
+      Name = name;
+      _instances.Add(this);
+      
+    }
+  }
+}
